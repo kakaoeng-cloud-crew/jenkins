1ad7870bbd8989394fa9d71bb51c129aec77b383
Yunsoonsang <tnstkd98@gmail.com>
2024-05-27 15:16:18 +0900

    final test code jenkins job - 0527 - 15:16

--- create_project.py
+++ create_project.py
@@ -4,6 +4,16 @@ import argparse
 from os import getenv
 from bson import ObjectId
 
+# subprocess 모듈
+def run_subprocess(command):
+    try:
+        result = subprocess.run(command, check=True, text=True, capture_output=True)
+        return result.stdout.strip()
+    except subprocess.CalledProcessError as e:
+        print(f"Command '{' '.join(command)}' failed.")
+        print("Error:", e.stderr.strip())
+        exit(1)
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--project_name', type=str, required=True)
 parser.add_argument('--project_id', type=str, required=True)
@@ -12,22 +22,9 @@ project_name = args.project_name
 project_id = args.project_id
 
 # 1. 네임스페이스 생성
-try:
-    # kubectl create namespace 명령어를 실행
-    result = subprocess.run(
-        ["sudo", "kubectl", "create", "namespace", project_name],
-        check=True,
-        text=True,
-        capture_output=True
-    )
-    # 명령어 실행 결과 출력
-    print(f"Namespace '{project_name}' created successfully.")
-    print("Output:", result.stdout)
-except subprocess.CalledProcessError as e:
-    # 명령어 실행 중 에러가 발생한 경우
-    print(f"Failed to create namespace '{project_name}'.")
-    print("Error:", e.stderr)
-    exit(1)
+print(f"Creating namespace '{project_name}'...")
+run_subprocess(["sudo", "kubectl", "create", "namespace", project_name])
+print(f"Namespace '{project_name}' created successfully.")
 
 # 2. 헬름 템플릿 불러와서 가져오기
 client = db.connect_to_db()
@@ -49,61 +46,44 @@ values_url = project_data.get('values_url')
 if not template_url or not values_url:
     print("Template URL or Values URL is missing in the project data.")
     exit(1)
-    
-print(template_url)
-print(values_url)
+
+print(f"Template URL: {template_url}")
+print(f"Values URL: {values_url}")
 
 # 저장 경로 설정
 template_file = f"/helm/{project_id}/{project_name}_template.tgz"
 values_file = f"/helm/{project_id}/{project_name}_values.yaml"
 
-print(template_file)
-print(values_file)
-
 # templates.tgz 파일 다운로드
-try:
-    result = subprocess.run(
-        ["sudo", "aws", "s3", "cp", template_url, template_file],
-        check=True,
-        text=True,
-        capture_output=True
-    )
-    print(f"Helm template file download successfully.")
-    print("Output:", result.stdout)
-except subprocess.CalledProcessError as e:
-    # 명령어 실행 중 에러가 발생한 경우
-    print(f"Failed to download Helm template file.")
-    print("Error:", e.stderr)
-    exit(1)
+print(f"Downloading Helm template file from {template_url} to {template_file}...")
+run_subprocess(["sudo", "aws", "s3", "cp", template_url, template_file])
+print("Helm template file downloaded successfully.")
+
 # values.yaml 다운로드
-try:
-    result = subprocess.run(
-        ["sudo", "aws", "s3", "cp", values_url, values_file],
-        check=True,
-        text=True,
-        capture_output=True
-    )
-    print(f"Helm values file download successfully.")
-    print("Output:", result.stdout)
-except subprocess.CalledProcessError as e:
-    # 명령어 실행 중 에러가 발생한 경우
-    print(f"Failed to download Helm values file.")
-    print("Error:", e.stderr)
-    exit(1)
+print(f"Downloading Helm values file from {values_url} to {values_file}...")
+run_subprocess(["sudo", "aws", "s3", "cp", values_url, values_file])
+print("Helm values file downloaded successfully.")
 
 # 4. 헬름 설치 명령어 실행
+print(f"Installing Helm chart for project '{project_name}'...")
+run_subprocess(["sudo", "helm", "install", project_name, template_file, "--values", values_file, "--namespace", project_name])
+print(f"Helm chart for project '{project_name}' installed successfully.")
+
+# 5. 로드밸랜서 DNS 이름 추출 및 DB 업데이트
+print(f"Retrieving load balancer DNS name for project '{project_name}'...")
+external_ip = run_subprocess(["kubectl", "get", "service", "-n", project_name, "-o", "jsonpath={.items[*].status.loadBalancer.ingress[*].hostname}"]).strip("'")
+print(f"External IP: {external_ip}")
+
+# 몽고 DB에 업데이트하기
 try:
-    result = subprocess.run(
-        ["sudo", "helm", "install", project_name, template_file, "--values", values_file, "--namespace", project_name],
-        check=True,
-        text=True,
-        capture_output=True
+    result = collection.update_one(
+        {"_id": ObjectId(project_id)},
+        {"$set": {"end_point": external_ip}}
     )
-    # 명령어 실행 결과 출력
-    print(f"Helm chart for project '{project_name}' installed successfully.")
-    print("Output:", result.stdout)
-except subprocess.CalledProcessError as e:
-    # 명령어 실행 중 에러가 발생한 경우
-    print(f"Failed to install Helm chart for project '{project_name}'.")
-    print("Error:", e.stderr)
+    if result.matched_count > 0:
+        print(f"Project '{project_id}' updated successfully with endpoint '{external_ip}'.")
+    else:
+        print(f"No project found with id '{project_id}' to update.")
+except Exception as e:
+    print(f"Error while updating project data: {e}")
     exit(1)
--- delete_project.py
+++ delete_project.py
@@ -1,9 +1,8 @@
-# import boto3
-# import db_utils as db  # db 접근을 위한 모듈
+import db_utils as db  # db 접근을 위한 모듈
 import subprocess
 import argparse
-# from os import getenv
-# from bson import ObjectId
+from os import getenv
+from bson import ObjectId
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--project_name', type=str, required=True)
@@ -12,9 +11,10 @@ args = parser.parse_args()
 project_name = args.project_name
 project_id = args.project_id
 
+# 1. 헬름 삭제하기
 try:
     result = subprocess.run(
-        ["sudo", "helm", "delete", project_name],
+        ["sudo", "helm", "delete", project_name, "--namespace", project_name],
         check=True,
         text=True,
         capture_output=True
@@ -27,6 +27,7 @@ except subprocess.CalledProcessError as e:
     print("Error:", e.stderr)
     exit(1)
 
+# 2. k8s 네임스페이스 삭제하기
 try:
     result = subprocess.run(
         ["sudo", "kubectl", "delete", 'namespace', project_name],
@@ -39,5 +40,36 @@ try:
 except subprocess.CalledProcessError as e:
     # 명령어 실행 중 에러가 발생한 경우
     print(f"Failed to delete namespace for k8s '{project_name}'.")
+    print("Error:", e.stderr)
+    exit(1)
+
+# 3. 데이터베이스 갱신
+try:
+    client = db.connect_to_db()
+    collection = db.get_collection(client, getenv("DB_NAME"), getenv("COL_NAME"))
+    # ObjectId로 변환하여 MongoDB에서 프로젝트 삭제
+    result = collection.delete_one({"_id": ObjectId(project_id)})
+    if result.deleted_count:
+        # 프로젝트가 성공적으로 삭제된 경우
+        return {"message": "Project successfully deleted"}
+    else:
+        # 프로젝트가 없는 경우 404 에러 반환
+        raise HTTPException(status_code=404, detail="Project not found")
+except Exception as e:
+    raise HTTPException(status_code=500, detail=str(e))
+
+# 4. /helm/{project_id} 디렉토리 삭제하기
+try:
+    path = f"/helm/{project_id}"
+    result = subprocess.run(
+        ["sudo", "rm", "-rf", path],
+        check=True,
+        text=True,
+        capture_output=True
+    )
+    print(f"Local directory '{path}' deleted successfully.")
+    print("Output:", result.stdout)
+except subprocess.CalledProcessError as e:
+    print(f"Failed to delete local directory '{local_helm_directory}'.")
     print("Error:", e.stderr)
     exit(1)
